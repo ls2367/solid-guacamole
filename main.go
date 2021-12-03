5f355f95d427086a05a546d9da6485b9d9cd9cb8
chbeck <christianillex97@yahoo.de>
2021-12-03 19:40:14 +0000

    liquid guacamole

--- main.go
+++ main.go
@@ -16,44 +16,11 @@ func getEnv(key string, fallback string) string {
 
 func main() {
 	http.HandleFunc("/", handlerindex)
-	http.HandleFunc("/1", handler1)
-	http.HandleFunc("/2", handler2)
-	http.HandleFunc("/3", handler3)
 	http.ListenAndServe(":"+getEnv("PORT", "8080"), nil)
 }
 
 func handlerindex(w http.ResponseWriter, r *http.Request) {
 	path := "project-reports.html"
-	t, err := template.ParseFiles(path)
-	if err != nil {
-		log.Fatalln(err)
-		return
-	}
-	t.Execute(w, nil)
-}
-
-func handler1(w http.ResponseWriter, r *http.Request) {
-	path := "project-reports.1.html"
-	t, err := template.ParseFiles(path)
-	if err != nil {
-		log.Fatalln(err)
-		return
-	}
-	t.Execute(w, nil)
-}
-
-func handler2(w http.ResponseWriter, r *http.Request) {
-	path := "project-reports.2.html"
-	t, err := template.ParseFiles(path)
-	if err != nil {
-		log.Fatalln(err)
-		return
-	}
-	t.Execute(w, nil)
-}
-
-func handler3(w http.ResponseWriter, r *http.Request) {
-	path := "project-reports.3.html"
 	t, err := template.ParseFiles(path)
 	if err != nil {
 		log.Fatalln(err)
